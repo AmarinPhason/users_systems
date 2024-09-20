6eae5d150059fdc9a1c5837a388bb6c54af17288
amarin phason <thedarksidest@gmail.com>
2024-09-20 12:47:30 +0700

    fix cookies

--- server/utils/cookieOptions.js
+++ server/utils/cookieOptions.js
@@ -1,6 +1,6 @@
 export const setCookieOptions = () => ({
   httpOnly: process.env.NODE_ENV === "development" ? false : true,
   secure: process.env.NODE_ENV === "development" ? false : true,
-  sameSite: process.env.NODE_ENV === "development" ? "lax" : "lax",
+  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none", // เปลี่ยนเป็น 'none' หากใช้ secure ใน production
   expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000), // 10 day
 });
